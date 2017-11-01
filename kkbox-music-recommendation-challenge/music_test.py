91201ab22d0d97d259d6ff6852c82e98ddd1ba1d
tusonggao <tusonggao@163.com>
2017-11-01 15:28:31 +0800

    add files

---
+++ kkbox-music-recommendation-challenge/music_test.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+
+
+
+
+if __name__=='__main__':
+    print('Hello world!')
+    train_df = pd.read_csv('./dataset/train.csv')
+    test_df = pd.read_csv('./dataset/test.csv')
+    print(train_df.head())
+    print(test_df.head())
+    
+    
+    
