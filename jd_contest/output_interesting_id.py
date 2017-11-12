3c394003b43d8e8745f393886e48706b5eb119fc
yushan007 <tusonggao@163.com>
2017-11-12 10:37:16 +0800

    change preprocessing code logic

--- jd_contest/output_interesting_id.py
+++ jd_contest/output_interesting_id.py
@@ -6,24 +6,31 @@ import matplotlib.pyplot as plt
 from sklearn import metrics
 from sklearn.model_selection import train_test_split
 
+def get_one():
+    global trade_df, merged_login_df
+    risk_id = trade_df[trade_df.is_risk==1]['id'].unique()
+    risk_login_df = merged_login_df[merged_login_df.id.isin(risk_id)]
+    count_df = risk_login_df.groupby(risk_login_df['id']).count()
+    print('count_df is ', count_df)
 
     
 if __name__=='__main__':
-    trade_df = pd.read_csv('./data/Risk_Detection_Qualification/t_trade.csv', index_col='rowkey', dtype={'id': np.str})
-    trade_test_df = pd.read_csv('./data/Risk_Detection_Qualification/t_trade_test.csv', index_col='rowkey', dtype={'id': np.str})
+    trade_df = pd.read_csv('./data/Risk_Detection_Qualification/t_trade.csv', 
+                           index_col='rowkey', dtype={'id': np.str})
+    trade_test_df = pd.read_csv('./data/Risk_Detection_Qualification/t_trade_test.csv', 
+                                index_col='rowkey', dtype={'id': np.str})
     dateparse = lambda x: pd.datetime.strptime(x, '%Y-%m-%d %H:%M:%S')
-    login_df = pd.read_csv('./data/Risk_Detection_Qualification/t_login.csv', index_col='log_id', 
-                       dtype={'id': np.str, 'timestamp': np.str},
-                       parse_dates=['time'], date_parser=dateparse)
-    login_test_df = pd.read_csv('./data/Risk_Detection_Qualification/t_login_test.csv', index_col='log_id', 
-                            dtype={'id': np.str, 'timestamp': np.str},
-                            parse_dates=['time'], date_parser=dateparse)
+    login_df = pd.read_csv('./data/Risk_Detection_Qualification/t_login.csv', 
+                           index_col='log_id', 
+                           dtype={'id': np.str, 'timestamp': np.str},
+                           parse_dates=['time'], date_parser=dateparse)
+    login_test_df = pd.read_csv('./data/Risk_Detection_Qualification/t_login_test.csv', 
+                                index_col='log_id', 
+                                dtype={'id': np.str, 'timestamp': np.str},
+                                parse_dates=['time'], date_parser=dateparse)
     merged_login_df = login_df.append(login_test_df)
     
-    X_train = trade_df[:, :-1]
-    y_train = trade_df[:, -1]
-    return X_train, y_train
-#    X_train, X_test, y_train, y_test = train_test_split(trade_df)
+    get_one()
     
     
     
