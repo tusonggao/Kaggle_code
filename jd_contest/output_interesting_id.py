c908091ac9b3dd974d8488e48b172523e8874fd4
yushan007 <tusonggao@163.com>
2017-11-13 10:53:01 +0800

    change preprocessing code logic

--- jd_contest/output_interesting_id.py
+++ jd_contest/output_interesting_id.py
@@ -13,7 +13,6 @@ from sklearn.model_selection import train_test_split
 #city_changed = login_sub_df['city']!=city_shifted
 #login_sub_df[city_changed]
 
-
 def get_one():
     global trade_df, merged_login_df
     risk_id = trade_df[trade_df.is_risk==1]['id'].unique()
@@ -22,40 +21,7 @@ def get_one():
     print('count_df is ', count_df)
 
     
-def get_change_city_min_elapse(login_df):
-    min_elapse = 10**5
-    for i in range(len(login_df)-1):
-        ip, city, time = login_df.iloc[i][['ip', 'city', 'time']]
-        ip_next, city_next, time_next = login_df.iloc[i+1][['ip', 'city', 'time']]
-        if city!=city_next:
-            elapse = (time_next - time)
-            elapse = np.timedelta64(elapse, 's')
-            elapse = elapse.astype('float')/60.0  # 计算间隔的分钟数，转换为float类型
-            min_elapse = min(min_elapse, elapse)        
-    return min_elapse
-    
 
-def get_neareast_N_login(N=20):  #得到最近20次登录切换城市的最小分钟数
-    global trade_df, merged_login_df
-    trade_df_new = trade_df.copy()
-    min_elapses = []
-    count = 0
-    print('starting computing')
-    start_t = time.time()
-    for index, row in trade_df.iterrows():
-        print('compute ', count)
-        count += 1
-        trade_time = row['time']
-        user_id = row['id']
-        login_sub_df = merged_login_df[merged_login_df.id==user_id].sort_values(by='time')
-        login_sub_df = login_sub_df[login_sub_df.time<=trade_time]
-        login_sub_df = login_sub_df.iloc[-N:, :]
-        elaspe_time = get_change_city_min_elapse(login_sub_df)
-        min_elapses.append(elaspe_time)
-    end_t = time.time()
-    print('cost time is ', end_t-start_t)
-    trade_df_new['change_city_min_elaspe'] = np.array(min_elapses)
-    trade_df_new.to_csv('change_city_min_elaspe.csv')
     
     
 def get_city_ip_login_num():
@@ -79,37 +45,45 @@ def get_city_ip_login_num():
     trade_df_new['city_num'] = city_count_df['city'][trade_df_new['id']].values
     trade_df_new.to_csv('ip_city_num.csv')
     
+#------------------------------------------------------------------------------------#
+#得到本次交易最近20次登录切换城市的最小分钟数
+def get_neareast_N_login(N=20):  
+    global trade_df, merged_login_df
+    trade_df_new = trade_df.copy()
+    min_elapses = []
+    count = 0
+    print('starting computing')
+    start_t = time.time()
+    for index, row in trade_df.iterrows():
+        print('compute ', count)
+        count += 1
+        trade_time = row['time']
+        user_id = row['id']
+        login_sub_df = merged_login_df[merged_login_df.id==user_id].sort_values(by='time')
+        login_sub_df = login_sub_df[login_sub_df.time<=trade_time]
+        login_sub_df = login_sub_df.iloc[-N:, :]
+        elaspe_time = get_change_city_min_elapse(login_sub_df)
+        min_elapses.append(elaspe_time)
+    end_t = time.time()
+    print('cost time is ', end_t-start_t)
+    trade_df_new['change_city_min_elaspe'] = np.array(min_elapses)
+    trade_df_new.to_csv('change_city_min_elaspe.csv')    
 
-def compute_elaspe_time(time1, time2): #计算间隔时间，以分钟计算 会包含小数点部分
-    if time1 > time2:
-        time1, time2 = time2, time1
-    elapse = (time2 - time1)
-    elapse = np.timedelta64(elapse, 's')
-    elapse = elapse.astype('float')/60.0  # 计算间隔的分钟数，转换为float类型
-    return elapse
+def get_change_city_min_elapse(login_df):
+    min_elapse = 10**5
+    for i in range(len(login_df)-1):
+        ip, city, time = login_df.iloc[i][['ip', 'city', 'time']]
+        ip_next, city_next, time_next = login_df.iloc[i+1][['ip', 'city', 'time']]
+        if city!=city_next:
+            elapse = (time_next - time)
+            elapse = np.timedelta64(elapse, 's')
+            elapse = elapse.astype('float')/60.0  # 计算间隔的分钟数，转换为float类型
+            min_elapse = min(min_elapse, elapse)        
+    return min_elapse
     
-
-def compute_last_time(device, ip, city, last_login_time, login_df):
-    device_t, ip_t, city_t = None, None, None
-    
-    device_t_series = login_df['time'][login_df.device==device]
-    ip_t_series = login_df['time'][login_df.ip==ip]
-    city_t_series = login_df['time'][login_df.city==city]
-    if len(device_t_series)>0:
-        device_t = device_t_series[-1]
-    if len(ip_t_series)>0:
-        ip_t = ip_t_series[-1]
-    if len(city_t_series)>0:
-        city_t = city_t_series[-1]
-
-    result_t = []
-    for t in (device_t, ip_t, city_t):
-        time_t = 10.**6 if t==None else compute_elaspe_time(t, last_login_time)
-        result_t.append(time_t)
-    return result_t
-
-    
-def get_last_login_device_ip_city_time(): # 最后一次登录的device ip city所登录距离现在的时间（以分钟计算）如果从来没有登录过 则为10**6
+#------------------------------------------------------------------------------------#
+#本次交易最后一次登录的device ip city所登录距离现在的时间（以分钟计算）如果从来没有登录过 则为10**6
+def get_last_login_device_ip_city_time(): 
     global trade_df, merged_login_df
     trade_df_new = trade_df.copy()
     device_new, ip_new, city_new = [], [], []
@@ -124,17 +98,16 @@ def get_last_login_device_ip_city_time(): # 最后一次登录的device ip city�
         login_sub_df = merged_login_df[merged_login_df.id==user_id].sort_values(by='time')
         login_sub_df = login_sub_df[login_sub_df.time<=trade_time]
         if len(login_sub_df)==0:  # 如果没有最后一次登录,则为10.**6
-            device_new.append[10.**6]
-            ip_new.append[10.**6]
-            city_new.append[10.**6]
+            device_new.append(10.**6)
+            ip_new.append(10.**6)
+            city_new.append(10.**6)
         else:
-            device, ip, city, last_login_t = login_sub_df.iloc[-1][['device', 'ip', 'city', 'time']]
+            device, ip, city = login_sub_df.iloc[-1][['device', 'ip', 'city']]
+            last_login_t = login_sub_df['time'].values[-1]
             device_t, ip_t, city_t = compute_last_time(device, ip, city, last_login_t, login_sub_df.iloc[:-1])
             device_new.append(device_t)
             ip_new.append(ip_t)
             city_new.append(city_t)
-        if count>10:
-            break
     end_t = time.time()
     print('cost time is ', end_t-start_t)
     trade_df_new['device_new'] = np.array(device_new)
@@ -142,8 +115,39 @@ def get_last_login_device_ip_city_time(): # 最后一次登录的device ip city�
     trade_df_new['city_new'] = np.array(city_new)
     trade_df_new.to_csv('last_login_device_ip_city_time.csv')
     
+def compute_last_time(device, ip, city, last_login_time, login_df):
+    device_t, ip_t, city_t = None, None, None
+    
+    device_t_series = login_df['time'][login_df.device==device]
+    ip_t_series = login_df['time'][login_df.ip==ip]
+    city_t_series = login_df['time'][login_df.city==city]
+    if len(device_t_series)>0:
+        device_t = device_t_series.values[-1]
+    if len(ip_t_series)>0:
+        ip_t = ip_t_series.values[-1]
+    if len(city_t_series)>0:
+        city_t = city_t_series.values[-1]
 
-def get_last_half_year_city_ip_num():
+    result_t = []
+    for t in (device_t, ip_t, city_t):
+        if t==None:
+            time_t = 10.**6 
+        else:
+            time_t = compute_elaspe_time(t, last_login_time)
+        result_t.append(time_t)
+    return result_t
+
+def compute_elaspe_time(time1, time2): #计算间隔时间，以分钟计算 会包含小数点部分
+    if time1 > time2:
+        time1, time2 = time2, time1
+    elapse = (time2 - time1)
+    elapse = np.timedelta64(elapse, 's')
+    elapse = elapse.astype('float')/60.0  # 计算间隔的分钟数，转换为float类型
+    return elapse
+
+#------------------------------------------------------------------------------------#
+#本次交易是否是当天第一次交易，是否是当天第一次登录
+def get_whether_first_trade_login():  
     global trade_df, merged_login_df
     print('starting computing')
     start_t = time.time()
@@ -161,6 +165,51 @@ def get_last_half_year_city_ip_num():
     trade_df_new['last_half_year_ip_num'] = ip_count_df['ip'][trade_df_new['id']].values
     trade_df_new['last_half_year_city_num'] = city_count_df['city'][trade_df_new['id']].values
     trade_df_new.to_csv('ip_city_num.csv')
+
+#------------------------------------------------------------------------------------#
+#本次交易距离上一次交易时间，距离上一次登录时间
+def get_last_login_trade_time_elapse():  
+    global trade_df, merged_login_df
+    print('starting computing')
+    start_t = time.time()
+    trade_df_new = trade_df.copy()
+    ip_count_df = merged_login_df[['ip']].groupby(merged_login_df['id']).unique().count()
+    city_count_df = merged_login_df[['city']].groupby(merged_login_df['id']).unique().count()
+    end_t = time.time()
+    print('cost time is ', end_t-start_t)
+    
+    trade_df_id_set = set(trade_df_new['id'])
+    merged_login_df_id_set = set(ip_count_df.index)
+    check = trade_df_id_set < merged_login_df_id_set
+    print('check is ', check)
+    
+    trade_df_new['last_half_year_ip_num'] = ip_count_df['ip'][trade_df_new['id']].values
+    trade_df_new['last_half_year_city_num'] = city_count_df['city'][trade_df_new['id']].values
+    trade_df_new.to_csv('ip_city_num.csv')
+    
+
+#------------------------------------------------------------------------------------#
+#得到该id在这之前的总交易次数、总登录次数、登录过的city数、ip数、device数
+def get_before_trade_login_city_ip_device_num():  
+    global trade_df, merged_login_df
+    print('starting computing')
+    start_t = time.time()
+    trade_df_new = trade_df.copy()
+    ip_count_df = merged_login_df[['ip']].groupby(merged_login_df['id']).unique().count()
+    city_count_df = merged_login_df[['city']].groupby(merged_login_df['id']).unique().count()
+    end_t = time.time()
+    print('cost time is ', end_t-start_t)
+    
+    trade_df_id_set = set(trade_df_new['id'])
+    merged_login_df_id_set = set(ip_count_df.index)
+    check = trade_df_id_set < merged_login_df_id_set
+    print('check is ', check)
+    
+    trade_df_new['last_half_year_ip_num'] = ip_count_df['ip'][trade_df_new['id']].values
+    trade_df_new['last_half_year_city_num'] = city_count_df['city'][trade_df_new['id']].values
+    trade_df_new.to_csv('ip_city_num.csv')    
+
+
     
     
     
@@ -184,7 +233,8 @@ if __name__=='__main__':
     print('load cost time: ', end_t-start_t)
     
 #    get_neareast_N_login()
-    get_city_ip_num()
+#    get_city_ip_num()
+    get_last_login_device_ip_city_time()
     
     
     
